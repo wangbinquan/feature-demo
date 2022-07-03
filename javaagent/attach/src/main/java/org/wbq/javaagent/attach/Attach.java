995ba62f7e2936e7199b3bca7d7ca0483c8d27c2
wangbinquan <wang_wbq@163.com>
2022-07-03 21:26:27 +0800

    javaagent-demo

--- javaagent/attach/src/main/java/org/wbq/javaagent/attach/Attach.java
+++ javaagent/attach/src/main/java/org/wbq/javaagent/attach/Attach.java
@@ -1,14 +1,15 @@
 package org.wbq.javaagent.attach;
 
-import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
 
-import java.io.IOException;
-
 public class Attach {
-    public static void main(String[] args) throws IOException, AttachNotSupportedException {
-        int pid = Integer.parseInt(args[1]);
-        VirtualMachine attach = VirtualMachine.attach(String.valueOf(pid));
-//        attach.loadAgent();
+    public static void main(String[] args) {
+        try {
+            int pid = Integer.parseInt(args[1]);
+            VirtualMachine attach = VirtualMachine.attach(String.valueOf(pid));
+            attach.loadAgent("javaagent-client-1.0-SNAPSHOT.jar");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ javaagent/client/src/main/java/org/wbq/javaagent/client/controller/Hello.java
@@ -0,0 +1,12 @@
+package org.wbq.javaagent.client.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
