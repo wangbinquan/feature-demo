91ec0f018337ef7d98614295fb0f6b6310d72064
wangbinquan <wang_wbq@163.com>
2022-07-03 23:00:18 +0800

    javaagent-demo

--- javaagent/attach/src/main/java/org/wbq/javaagent/attach/Attach.java
+++ javaagent/attach/src/main/java/org/wbq/javaagent/attach/Attach.java
@@ -1,13 +1,20 @@
 package org.wbq.javaagent.attach;
 
 import com.sun.tools.attach.VirtualMachine;
+import com.sun.tools.attach.VirtualMachineDescriptor;
 
 public class Attach {
     public static void main(String[] args) {
         try {
-            int pid = Integer.parseInt(args[1]);
-            VirtualMachine attach = VirtualMachine.attach(String.valueOf(pid));
-            attach.loadAgent("javaagent-agent-1.0-SNAPSHOT-jar-with-dependencies.jar");
+            for(VirtualMachineDescriptor virtualMachine: VirtualMachine.list()) {
+                if ("org.wbq.javaagent.client.ApplicationMain".equals(virtualMachine.displayName())) {
+                    int pid = Integer.parseInt(virtualMachine.id());
+                    VirtualMachine attach = VirtualMachine.attach(String.valueOf(pid));
+                    attach.loadAgent("./javaagent/agent/target/javaagent-agent-1.0-SNAPSHOT-jar-with-dependencies.jar");
+                    attach.detach();
+                    return;
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
