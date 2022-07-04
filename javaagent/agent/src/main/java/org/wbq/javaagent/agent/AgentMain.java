5f452050161b40055c6a0cdec8bd59e19f2b16d5
wangbinquan <wang_wbq@163.com>
2022-07-04 22:09:46 +0800

    javaagent-demo

--- javaagent/agent/src/main/java/org/wbq/javaagent/agent/AgentMain.java
+++ javaagent/agent/src/main/java/org/wbq/javaagent/agent/AgentMain.java
@@ -1,7 +1,11 @@
 package org.wbq.javaagent.agent;
 
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
 import org.wbq.javaagent.agent.transformer.Transformer;
 
+import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
 
@@ -16,7 +20,7 @@ public class AgentMain {
         instrumentation.addTransformer(new Transformer());
     }
 
-    public static void agentmain(String arg, Instrumentation instrumentation) {
+    public static void agentmain_bak(String arg, Instrumentation instrumentation) {
         System.out.println("Call AgentMain Func");
 
         instrumentation.addTransformer(new Transformer(), true);
@@ -24,6 +28,24 @@ public class AgentMain {
         try {
             instrumentation.retransformClasses(Class.forName("org.wbq.javaagent.client.controller.Hello"));
         } catch (UnmodifiableClassException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void agentmain(String arg, Instrumentation instrumentation) {
+        try {
+            final ClassPool classPool = ClassPool.getDefault();
+            final CtClass clazz = classPool.get("org.wbq.javaagent.client.controller.Hello");
+            CtMethod ctMethod = clazz.getDeclaredMethod("hello");
+            String methodBody = "return \"Hello Agent Hack World v2\";";
+            ctMethod.setBody(methodBody);
+            // 返回字节码，并且detachCtClass对象
+            byte[] byteCode = clazz.toBytecode();
+            //detach的意思是将内存中曾经被javassist加载过的Date对象移除，如果下次有需要在内存中找不到会重新走javassist加载
+            clazz.detach();
+            ClassDefinition classDefinition = new ClassDefinition(Class.forName("org.wbq.javaagent.client.controller.Hello"), byteCode);
+            instrumentation.redefineClasses(classDefinition);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
